e664801cf0f7ce16188136010356df1e69df3c38
BeanZero <emperor220@163.com>
2019-04-21 20:57:39 +0800

    commit cloud-eureka

---
+++ cloud-eureka/src/main/java/com/scia/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.scia.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author BeanZero
+ * @date 2019-04-21
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
