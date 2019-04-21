16629bbd22805489c3e9af7255320488bea6332b
BeanZero <emperor220@163.com>
2019-04-21 18:12:30 +0800

    commit cloud-config

---
+++ cloud-config/src/main/java/com/scia/cloud/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.scia.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author BeanZero
+ * @date 2019-04-21
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
