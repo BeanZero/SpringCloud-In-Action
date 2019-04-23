07ab550f9c4d0fb9f95d65931c4944e0df5a909e
BeanZero <emperor220@163.com>
2019-04-23 15:38:09 +0800

    新增链路监控：cloud-zipkin模块

--- cloud-config/docker_compose.sh
+++ cloud-config/docker_compose.sh
@@ -4,22 +4,22 @@
 
 # 构建并启动容器
 config_version=1.0.0 \
-eureka_host_ip=10.2.230.108 \
+eureka_host_ip=192.168.1.100 \
 docker-compose up -d
 
 # 重启容器
 #config_version=1.0.0 \
-#eureka_host_ip=10.2.230.108 \
+#eureka_host_ip=192.168.1.100 \
 #config_version=1.0.0 docker-compose restart
 
 # 停止容器
 #config_version=1.0.0 \
-#eureka_host_ip=10.2.230.108 \
+#eureka_host_ip=192.168.1.100 \
 #config_version=1.0.0 docker-compose stop
 
 # 卸载容器
 #config_version=1.0.0 \
-#eureka_host_ip=10.2.230.108 \
+#eureka_host_ip=192.168.1.100 \
 #config_version=1.0.0 docker-compose down
 
 
