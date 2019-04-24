002ab1b3b0ac87227f4883ca86e3cb8ad5f1b818
BeanZero <emperor220@163.com>
2019-04-24 22:46:47 +0800

    新增服务：service-provider模块

--- cloud-config/docker_compose.sh
+++ cloud-config/docker_compose.sh
@@ -2,10 +2,14 @@
 
 # config_version对应images的版本号，例如：scia/cloud-config:1.0.0
 
-# 构建并启动容器
 config_version=1.0.0 \
 eureka_host_ip=192.168.1.100 \
-docker-compose up -d
+docker-compose $@
+
+# 构建并启动容器
+#config_version=1.0.0 \
+#eureka_host_ip=192.168.1.100 \
+#docker-compose up -d
 
 # 重启容器
 #config_version=1.0.0 \
